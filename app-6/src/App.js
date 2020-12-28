0853527c8d41f2764093be56709351ecb7399244
Teddy-Hung <tedhungkk@gmail.com>
2020-12-27 18:07:53 -0800

    done app-6

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import ToDo from './ToDo'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super()
+
+    this.state={
+      userInput: '',
+      listOfItems: []
+    }
+  }
+
+  handleChange(e){
+    this.setState({userInput: e})
+  }
+  
+  handleAdd(item){
+    let arr = this.state.listOfItems
+    arr.push(item)
+    this.setState({listOfItems: arr})
+  }
+
+  render(){
+    // const listItems = this.state.listOfItems.map((e, i) => <h2 key={i}> {e} </h2>)
+    const listItems = this.state.listOfItems.map((e, i) => <ToDo key= {i} item= {e}/>)
+    return(
+      <section class= 'App'>
+        <h1>My to-do list</h1>
+        <input placeholder= 'Enter new task' onChange= {(e) => this.handleChange(e.target.value)}></input>
+        <button class= 'AddButton' onClick = {() => this.handleAdd(this.state.userInput)}>Add</button>
+        <span class= 'list'>{listItems}</span>
+      </section>
+    )
+  }
 }
 
 export default App;
---
+++ app-6/src/ToDo.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class ToDo extends Component{
+    render(){
+        return <h2>{this.props.item}</h2>
+    }
+
+}
+export default ToDo
