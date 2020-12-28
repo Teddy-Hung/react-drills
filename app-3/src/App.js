a2ba4bf62e90acc31ccbafb1a1a19deadba713fc
Teddy-Hung <tedhungkk@gmail.com>
2020-12-27 17:20:40 -0800

    fixed wrong project

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,41 +1,46 @@
-import React, {Component} from 'react';
+import React from 'react';
+import {Component} from 'react'
 import './App.css';
 
-class App extends Component{
+class App extends Component {
+
   constructor(){
     super()
 
-    this.state = {      
-      username: '',
-      password: ''
+    this.state = {
+      arr: ['spaghetti', 'ice cream', 'sushi', 'bologna', 'cheese'],
+      userInput: '',
+      filteredArr: []
     }
-
-    //Why do we need this to work?
-    // this.handleLogin = this.handleLogin.bind(this)
   }
 
-  handleUserName(e){
-    this.setState({username: e})
+  handleChange(e){
+    this.setState({userInput: e})
   }
 
-  handlePassWord(e){
-    this.setState({password: e})
-  }
-
-  handleLogin(username, password){
-    alert(`Username: ${username} Password: ${password}`)
-  }
+  // filterStrings(keyword){
+  //   let array = this.state.arr.map()
+  //   for(let i=0; i<array.length; i++){
+  //     if(array.includes(keyword)){
+  //       this.filteredArr.push(array[i])
+  //     }
+  //   }
+  //   this.setState({filteredArr: array})
+  // }
 
   render(){
-    return(
-      <section className='App'>
-        <input class='UserName' onChange= {(e) => this.handleUserName(e.target.value)}></input>
-        <input class='Password' onChange= {(e) => this.handlePassWord(e.target.value)}></input>
-        <button class='login' onClick= {() => this.handleLogin(this.state.username, this.state.password)}>Login</button>
+    let listItems = this.state.arr.filter((e, i) => {
+      return e.includes(this.state.userInput)
+    }).map((e)=> {
+      return <h2>{e}</h2>
+    })
+    return (
+      <section class= 'App'>
+        <input class= 'InputBox' onChange = {(e) => this.handleChange(e.target.value)}></input>
+        {listItems}
       </section>
     )
   }
-
 }
 
 export default App;
