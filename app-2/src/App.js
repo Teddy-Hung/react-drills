fe7e22d9166313e6bb96a9be1a74b30c53877826
Teddy-Hung <tedhungkk@gmail.com>
2020-12-27 16:30:12 -0800

    done

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -8,14 +8,35 @@ class App extends Component {
     super()
 
     this.state = {
-      arr: ['spaghetti', 'ice cream', 'sushi', 'bologna', 'cheese']
+      arr: ['spaghetti', 'ice cream', 'sushi', 'bologna', 'cheese'],
+      userInput: '',
+      filteredArr: []
     }
   }
 
+  handleChange(e){
+    this.setState({userInput: e})
+  }
+
+  // filterStrings(keyword){
+  //   let array = this.state.arr.map()
+  //   for(let i=0; i<array.length; i++){
+  //     if(array.includes(keyword)){
+  //       this.filteredArr.push(array[i])
+  //     }
+  //   }
+  //   this.setState({filteredArr: array})
+  // }
+
   render(){
-    const listItems = this.state.arr.map((e, i) => <h2 key={i}>{e}</h2>)
+    let listItems = this.state.arr.filter((e, i) => {
+      return e.includes(this.state.userInput)
+    }).map((e)=> {
+      return <h2>{e}</h2>
+    })
     return (
       <section>
+        <input class= 'InputBox' onChange = {(e) => this.handleChange(e.target.value)}></input>
         {listItems}
       </section>
     )
