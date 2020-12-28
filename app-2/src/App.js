6f47d4522fbfc0cf1f216c26978c2ba647b199f7
Teddy-Hung <tedhungkk@gmail.com>
2020-12-27 17:17:57 -0800

    fixed wrong project issue

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -9,8 +9,6 @@ class App extends Component {
 
     this.state = {
       arr: ['spaghetti', 'ice cream', 'sushi', 'bologna', 'cheese'],
-      userInput: '',
-      filteredArr: []
     }
   }
 
@@ -18,25 +16,10 @@ class App extends Component {
     this.setState({userInput: e})
   }
 
-  // filterStrings(keyword){
-  //   let array = this.state.arr.map()
-  //   for(let i=0; i<array.length; i++){
-  //     if(array.includes(keyword)){
-  //       this.filteredArr.push(array[i])
-  //     }
-  //   }
-  //   this.setState({filteredArr: array})
-  // }
-
   render(){
-    let listItems = this.state.arr.filter((e, i) => {
-      return e.includes(this.state.userInput)
-    }).map((e)=> {
-      return <h2>{e}</h2>
-    })
+    const listItems = this.state.arr.map((e, i) => <h2 key={i}>{e}</h2>)
     return (
       <section>
-        <input class= 'InputBox' onChange = {(e) => this.handleChange(e.target.value)}></input>
         {listItems}
       </section>
     )
